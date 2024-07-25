c85fa08d2541ace7918901ac73e260c6145e8660
Sakshi Yadwade-22 <159874331+SakshiYadwade-22@users.noreply.github.com>
2024-07-24 23:01:23 -0700

    Add.java

---
+++ Add.java
@@ -0,0 +1,22 @@
+import java.util.scanner
+class Add
+{
+	public static void main(String[]args);
+{
+	int a,b,sum;
+	Scanner s c= new scanner(System.in);
+	System.out.println("Enter the first number: ");
+	a=sc.nextInt();
+	System.out.println("Enter the second number: ");
+	b=sc.nextInt();
+	sum=a+b;
+	System.out.println("Addition is : " +sum);
+
+}
+}
+	
+
+
+	
+	
+	
